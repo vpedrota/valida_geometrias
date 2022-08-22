c8746c2cc972a94414c3ab3d8b143c6321c3b64a
victor <victorpedrota@gmail.com>
2022-08-22 08:14:59 -0300

    Novo módulo

--- source/app.py
+++ source/app.py
@@ -2,54 +2,7 @@ import psycopg2
 import jsonschema
 import json
 from flask import Flask, request
-
-schema = {
-    "type": "object",
-    "properties": {
-        "type": {
-                    "enum" : ["Polygon", "MultiPolygon", "LineString", "Point", "MultiPoint", "MultiLineString", "GeometryCollection"]
-                },
-        "coodinates": { "type": "array"}
-    },
-    "required": ["type", "coordinates"]
-}
-
-
-def json_schema(request):
-    """_summary_
-
-    Args:
-        request (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    v = jsonschema.Draft7Validator(schema, format_checker=jsonschema.draft7_format_checker)
-
-    errorMessage = {
-        "type": None,
-        "title": None,
-        "status": None,
-        "detail": None,
-    }
-    
-    try:
-        v.validate(request)
-        return None
-    except (jsonschema.exceptions.ValidationError):
-        errorMessage['campos'] = []
-        for error in sorted(v.iter_errors(request), key=str):
-            string = str(error.message)
-            string = string.replace("is a required property", "é um campo obrigatório")
-            string = string.replace("is not of type", "não é do tipo")
-            string = string.replace("is not one of", "não é de nenhum dos tipos ")
-            errorMessage['campos'].append(string)
-
-        errorMessage['type'] = "https://dominio.visiona/example-error"
-        errorMessage['status'] = 400
-        errorMessage['detail'] = "A requisição não possui os campos obrigatórios para processamento ou valores estão fora do formato especificado"
-        errorMessage['title'] = "A requisição não pode ser processada devido a validação de campos"
-        return errorMessage
+from core_functions import *
 
 def create_app():
 
